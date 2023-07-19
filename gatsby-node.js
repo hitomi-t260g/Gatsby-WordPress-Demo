5076991792f8fc9d87c4ae4611e0f307a415f7d5
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2023-07-20 03:19:53 +0900

    add HomePage

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,9 @@
 const path = require("path");
+const {
+  assignIds,
+  assignGatsbyImage,
+} = require("@webdeveducation/wp-block-tools");
+const fs = require("fs");
 
 exports.createPages = async ({ actions, graphql }) => {
   const pageTemplate = path.resolve("src/templates/page.js");
@@ -6,6 +11,9 @@ exports.createPages = async ({ actions, graphql }) => {
 
   const { data } = await graphql(`
     query AllPagesQuery {
+      wp {
+        themeStylesheet
+      }
       allWpPage {
         nodes {
           databaseId
@@ -16,11 +24,22 @@ exports.createPages = async ({ actions, graphql }) => {
     }
   `);
 
+  try {
+    fs.writeFileSync("./public/themeStylesheet.css", data.wp.themeStylesheet);
+  } catch (e) {}
+
   for (let i = 0; i < data.allWpPage.nodes.length; i++) {
     const page = data.allWpPage.nodes[i];
+    let blocks = page.blocks;
+    blocks = assignIds(blocks);
+    blocks = await assignGatsbyImage({ blocks, graphql, coreMediaText: true });
+
     createPage({
       path: page.uri,
       component: pageTemplate,
+      context: {
+        blocks,
+      },
     });
   }
 };
