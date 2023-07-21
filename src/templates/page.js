7d99b24c16351a838ce20427667bea7b6d08c33c
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2023-07-22 08:22:17 +0900

    add link in page.js

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BlockRendererProvider } from "@webdeveducation/wp-block-tools";
 import { blockRenderComponents } from "../config/blockRenderComponents";
+import { Link } from "gatsby";
 
 const Page = (props) => {
   console.log("PAGE PROPS:", props);
@@ -9,6 +10,17 @@ const Page = (props) => {
       <BlockRendererProvider
         allBlocks={props.pageContext.blocks}
         renderComponent={blockRenderComponents}
+        siteDomain={process.env.GATSBY_WP_URL}
+        customInternalLinkComponent={(
+          { children, internalHref, className },
+          index
+        ) => {
+          return (
+            <Link to={internalHref} className={className} key={index}>
+              {children}
+            </Link>
+          );
+        }}
       />
     </>
   );
